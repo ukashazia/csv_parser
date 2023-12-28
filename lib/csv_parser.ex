820079071fc454144fde5b05eff37433d27d6a1a
Ukasha Zia <ukashazia41@gmail.com>
2023-12-29 02:31:52 +0500

    complete

---
+++ lib/csv_parser.ex
@@ -0,0 +1,22 @@
+defmodule CsvParser do
+  @moduledoc """
+  Documentation for `CsvParser`.
+  """
+
+  @doc """
+  Read data from CSV
+
+  ## Examples
+
+      iex> CsvParser.parse("valid_csv.csv")
+
+  """
+  def parse(file_path, options \\ []) do
+    if File.exists?(file_path) do
+      file_path
+      |> File.stream!()
+      |> CSV.decode!([headers: true, validate_row_length: true] ++ options)
+      |> Enum.map(& &1)
+    end
+  end
+end
---
+++ test/csv_parser_test.exs
@@ -0,0 +1,33 @@
+defmodule CsvParserTest do
+  use ExUnit.Case
+  doctest CsvParser
+
+  test "parses the csv" do
+    assert CsvParser.parse("./examples/example.csv") == [
+             %{
+               "Department" => "Engineering",
+               "Education_Level" => "Masters",
+               "Email" => "john.smith@example.com",
+               "ID" => "101",
+               "Location" => "New York",
+               "Manager_ID" => "201",
+               "Name" => "John Smith",
+               "Phone" => "(123) 456-7890",
+               "Salary" => "80000",
+               "Start_Date" => "2022-05-15"
+             },
+             %{
+               "Department" => "Marketing",
+               "Education_Level" => "Bachelors",
+               "Email" => "alice.johnson@example.com",
+               "ID" => "102",
+               "Location" => "Los Angeles",
+               "Manager_ID" => "202",
+               "Name" => "Alice Johnson",
+               "Phone" => "(234) 567-8901",
+               "Salary" => "70000",
+               "Start_Date" => "2022-03-20"
+             }
+           ]
+  end
+end
